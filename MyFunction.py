31d5b7a0a05431d488f823b50ab231fb68c7eb8e
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-17 09:30:40 +0100

    My Function Updated

--- MyFunction.py
+++ MyFunction.py
@@ -1,7 +1,8 @@
 
 
-def my_function():
-    print("This is my function!")
-    print("A second String.")
+def my_function(str1, str2):
+    print(str1)
+    print(str2)
 
-my_function()
+my_function("This is arg 1","This is arg 2")
+my_function("Hello","World")
