d614d8d4ae94369f45fb27dbc34e8b48c206fb1a
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-17 10:16:34 +0100

    Organizing

--- MyFunction.py
+++ MyFunction.py
@@ -4,14 +4,26 @@ def my_function(str1, str2):
     print(str1)
     print(str2)
 
+my_function("This is arg 1","This is arg 2")
+my_function("Hello","World")
+
 def my_function2(int1,int2):
     print(int1+int2)
+
+my_function2(5,5)
 
 #function with default arguments
 def print_something(name = "Someone",age = "Unknown"):
     print("My name is",name,"and my age is",age)
 
-#infinite arguments
+#passing all args
+print_something("João",31)
+
+#Keyword arguments
+print_something(age = 27)
+print_something(name = "Joana")
+
+#infinite (or flexible) arguments
 def print_people(*people):
     for person in people:
         print("This person is",person)
@@ -26,14 +38,3 @@ math1 = do_math(5,7)
 math2 = do_math(11,34)
 
 print("First sum is", math1,"and the second sum is",math2)
-
-
-#Keyword arguments
-print_something(age = 27)
-print_something(name = "Joana")
-
-print_something("João",31)
-
-my_function("This is arg 1","This is arg 2")
-my_function("Hello","World")
-my_function2(5,5)
