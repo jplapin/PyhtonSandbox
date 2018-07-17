1e264f12c0184fdbdbbca1fc71d0a968eb26aad3
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-17 09:57:41 +0100

    Function modifications and Function with default arguments

--- MyFunction.py
+++ MyFunction.py
@@ -7,6 +7,11 @@ def my_Function(str1, str2):
 def my_Function2(int1,int2):
     print(int1+int2)
 
+def print_something(name = "Someone",age = "Unknown"):
+    print("My name is",name,"and my age is",age)
+
+print_something("João",31)
+
 my_Function("This is arg 1","This is arg 2")
 my_Function("Hello","World")
 my_Function2(5,5)
