18c46009e7345a0cfec5c533fd5633a09bd9fb7d
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-17 10:04:26 +0100

    Keyword Arguments

--- MyFunction.py
+++ MyFunction.py
@@ -1,17 +1,22 @@
 
 
-def my_Function(str1, str2):
+def my_function(str1, str2):
     print(str1)
     print(str2)
 
-def my_Function2(int1,int2):
+def my_function2(int1,int2):
     print(int1+int2)
 
+#function with default arguments
 def print_something(name = "Someone",age = "Unknown"):
     print("My name is",name,"and my age is",age)
 
+#Keyword arguments
+print_something(age = 27)
+print_something(name = "Joana")
+
 print_something("João",31)
 
-my_Function("This is arg 1","This is arg 2")
-my_Function("Hello","World")
-my_Function2(5,5)
+my_function("This is arg 1","This is arg 2")
+my_function("Hello","World")
+my_function2(5,5)
