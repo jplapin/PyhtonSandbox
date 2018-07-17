553d900025092ea6161621275472832de33c9a4d
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-17 09:46:32 +0100

    My Function Changed

--- MyFunction.py
+++ MyFunction.py
@@ -1,8 +1,12 @@
 
 
-def my_function(str1, str2):
+def my_Function(str1, str2):
     print(str1)
     print(str2)
 
-my_function("This is arg 1","This is arg 2")
-my_function("Hello","World")
+def my_Function2(int1,int2):
+    print(int1+int2)
+
+my_Function("This is arg 1","This is arg 2")
+my_Function("Hello","World")
+my_Function2(5,5)
