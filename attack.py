d3437009c43c8918f9e8416235c41bda2a2f7034
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-23 20:05:17 +0100

    class example

--- attack.py
+++ attack.py
@@ -1,6 +1,29 @@
 import random
 
 
+#simple class exemple
+class Enemy:
+    atkl = 60
+    atkh = 80
+
+    def getAtkl(self):
+        print(self.atkl)
+
+    def getAtkh(self):
+        print(self.atkh)
+    
+#instanciates the class 
+#2 enemies 
+enemy1 = Enemy()
+enemy1.getAtkh()
+enemy1.getAtkl()
+
+enemy2 = Enemy()
+enemy2.getAtkh()
+enemy2.getAtkl()
+
+
+
 playerhp = 260 #player health points
 enemytkl = 60 #enemy attack low
 enemytkh = 80 #enemy attack high
