899c235a2db957a2254a23c02db1fadcb76e516e
jplapin <jp.da.costa.coelho@gmail.com>
2018-07-23 20:24:03 +0100

    defining the enemy hp

--- attack.py
+++ attack.py
@@ -4,27 +4,34 @@ import random
 #simple class example
 class Enemy:
 
+    #every enemy has a hp of 200
+    hp = 200
+
     #runs when the class is been instantiated or when the object is created
     def __init__(self, atkl, atkh):
         self.atkl = atkl
         self.atkh = atkh
 
     def getAtkl(self):
-        print(self.atkl)
+        print("Atack is",self.atkl)
 
     def getAtkh(self):
-        print(self.atkh)
+        print("Atack is",self.atkh)
+
+    def getHp(self):
+        print("Hp is",self.hp)
     
 #instantiates the class 
 #2 enemies with diferent attacks
 enemy1 = Enemy(40,49)
 enemy1.getAtkh()
 enemy1.getAtkl()
+enemy1.getHp()
 
 enemy2 = Enemy(75,90)
 enemy2.getAtkh()
 enemy2.getAtkl()
-
+enemy2.getHp()
 
 playerhp = 260 #player health points
 enemytkl = 60 #enemy attack low
