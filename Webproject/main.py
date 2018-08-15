e5e7de4ef6b1ae8739c5cfbf473f61d0b03eb742
jplapin <jp.da.costa.coelho@gmail.com>
2018-08-15 10:34:08 +0100

    Basic Template

---
+++ Webproject/main.py
@@ -0,0 +1,19 @@
+import web
+
+# the (.*) means that accept everything after the slash
+urls = {
+    "/(.*)/(.*)", "index"
+}
+
+# app = web.application(urls, globals())
+render = web.template.render("resources/")
+
+
+class index:
+    def GET(self, name, age):
+        return render.main(name, age)
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
