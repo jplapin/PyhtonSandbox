656f052b833a391b4f060e016b047f5872c52d77
João Pedro da Costa Coelho <jp.da.costa.coelho@gmail.com>
2018-10-23 13:47:06 +0100

    bug correction

--- Webscrapery/main.py
+++ Webscrapery/main.py
@@ -6,7 +6,7 @@ params = {"q": search_term}
 response = requests.get("https://www.google.pt/search", params=params)
 
 soup = BeautifulSoup(response.text, "html.parser")
-# print(soup.prettify)
+#print(soup.prettify)
 
 results = soup.find("div", {"id": "ires"})
 links = results.findAll("h3", {"class": "r"})
@@ -18,7 +18,8 @@ for summary, item in zip(summaries, links):
     item_href = item.find("a").attrs["href"][7:].split('&')
     if item_text and item_href and item_href[0].startswith("http"):
         print("="*100)
-        print("Summary:", summary.find("span", {"class": "st"}).text)
+        
+        print("Summary:", summary.find("span", {"class": "st"}).text if summary.find("span", {"class": "st"}) else "")
         print("Text:", item_text)
         print("Link:", item_href[0])
         print("="*100)
